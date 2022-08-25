6dda68d3b02a4cdfbae0d048ec0c79b87c88715d
Timoh97 <timohmugendi@gmail.com>
2022-08-25 10:26:25 +0300

    create password box

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,6 +6,16 @@ export default class Login extends React.Component {
         return (
 <div className='col-lg-9'>\
 <h4 className='m-1 p-2 border-bottom'>Login</h4>
+{/* Email starts */}
+<div className='form-group form-row'>
+<label className='col-lg-4'>Email</label>
+<input type='text' className='form-control'/>
+</div>
+{/* Email ends */}
+<div className='form-group form-row'>
+<label className='col-lg-4'>Password</label>
+<input type='password' className='form-control'/>
+</div>
 </div>
         )
     }
