98100490079045eb65b760b613315b42934bf25b
Timoh97 <timohmugendi@gmail.com>
2022-08-25 10:35:56 +0300

    add event handlers

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default class Login extends React.Component {
     state = {
-        email: '',
-        password: ''
+        email: 'qbnnn',
+        password: 'hh78'
     }
     render (){
         return (
@@ -13,12 +13,12 @@ export default class Login extends React.Component {
 {/* Email starts */}
 <div className='form-group form-row'>
 <label className='col-lg-4'>Email</label>
-<input type='text' value={this.state.email}  className='form-control'/>
+<input type='text' value={this.state.email} onChange={(event)=>{event.target.value}} className='form-control'/>
 </div>
 {/* Email ends */}
 <div className='form-group form-row'>
 <label className='col-lg-4'>Password</label>
-<input type='password' value={this.state.password} className='form-control'/>
+<input type='password'  value={this.state.password} className='form-control' onChange={(event)=>{event.target.value}} />
 </div>
 </div>
         )
